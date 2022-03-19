8de975b94d48b0014f7f4986037c2f94bd0fbaea
alahyaoui <54895@etu.he2b.be>
2022-03-20 00:19:00 +0100

    Update rainbow attack functions

--- attack/rainbow_attack.hpp
+++ attack/rainbow_attack.hpp
@@ -10,85 +10,83 @@
 
 namespace rainbow {
 
-std::string generate_tail(std::string hash, int pwdSize, int pos ,int nbOfReduction){ 
+int size;
+
+std::string generate_tail(std::string hash, const int & pos ,const int & nb_of_reductions){ 
     std::string tail;
 
-    for(size_t i = pos; i < nbOfReduction; i++){
-        tail = reduce(hash, i, pwdSize);
+    for(size_t i = pos; i < nb_of_reductions; i++){
+        tail = reduce(hash, i, size);
         hash = sha256(tail);
     }
 
-    tail = reduce(hash, nbOfReduction, pwdSize);
+    tail = reduce(hash, nb_of_reductions, size);
     return tail;
 }
 
-std::string find_in_chain(const std::string & head, const std::string & hashToCrack, const int & nbOfReduction){ 
+std::string find_in_chain(const std::string & head, const std::string & hash_to_crack, const int & nb_of_reductions){ 
     std::string pwd = head;
     std::string hash;
 
-    int pwdSize = head.size();
-
-    for(size_t i = 1; i < nbOfReduction + 1; i++){
+    for(size_t i = 1; i < nb_of_reductions + 1; i++){
         hash = sha256(pwd);
-        if(strcmp(hashToCrack.c_str(), hash.c_str()) == 0){
+        if(strcmp(hash_to_crack.c_str(), hash.c_str()) == 0){
             return pwd;
         }
-        pwd = reduce(hash, i, pwdSize); 
+        pwd = reduce(hash, i, size); 
     }
 
     return "";
 }
 
-std::string find_in_tail_file(const std::string & pwd , const std::string & hash, const int & nbOfReduction ,const std::string & if_heads_tails){
-    std::ifstream heads_tails_file;
-    heads_tails_file.open(if_heads_tails);
+std::string find_in_rainbow_table_file(const std::string & tail_generated, const std::string & hash_to_crack, const int & nb_of_reductions ,const std::string & rainbow_table_path){
+    std::ifstream rainbow_table_file;
+    rainbow_table_file.open(rainbow_table_path);
 
-    if(heads_tails_file.is_open()){
+    if(rainbow_table_file.is_open()){
         std::string head_tail;
         std::string head;
         std::string tail;
 
-        heads_tails_file.clear();
-        heads_tails_file.seekg(0);
+        rainbow_table_file.clear();
+        rainbow_table_file.seekg(0);
 
-        while(std::getline(heads_tails_file, head_tail)){
+        while(std::getline(rainbow_table_file, head_tail)){
             int lineSeparator = head_tail.find(":");
 
             tail = head_tail.substr(lineSeparator + 1);
-            if(strcmp(pwd.c_str(), tail.c_str()) == 0){  
+            if(strcmp(tail_generated.c_str(), tail.c_str()) == 0){  
                 head = head_tail.substr(0, lineSeparator);
 
-                std::string pwdInChain = find_in_chain(head, hash, nbOfReduction);
-                if(pwdInChain != "")
-                    return pwdInChain;
+                std::string pwd_in_chain = find_in_chain(head, hash_to_crack, nb_of_reductions);
+                if(pwd_in_chain != "")
+                    return pwd_in_chain;
             }
         }
 
-        heads_tails_file.close();
-
+        rainbow_table_file.close();
     }
     else
         throw std::runtime_error("Input files could not be opened");
     return "";
 }
 
-bool attack(std::string hash, const int & pwd_size, const int & nb_of_reductions, const std::string & if_heads_tails){
+bool attack(std::string hash_to_crack, const int & pwd_size, const int & nb_of_reductions, const std::string & rainbow_table_path){
     std::string tail;
     std::string pwd;
 
-    bool is_found = false;
-    
-    //int count = 1;
+    size = pwd_size;
+    bool is_found = false;    
     int i = nb_of_reductions;
     while(is_found == false && i > 0){
-        tail = generate_tail(hash, pwd_size, i, nb_of_reductions);
-        pwd = find_in_tail_file(tail, hash, nb_of_reductions, if_heads_tails);
+        tail = generate_tail(hash_to_crack, i, nb_of_reductions);
+        pwd = find_in_rainbow_table_file(tail, hash_to_crack, nb_of_reductions, rainbow_table_path);
 
        
        if(pwd != ""){
-            if(strcmp(sha256(pwd).c_str(), hash.c_str()) == 0){
+            if(strcmp(sha256(pwd).c_str(), hash_to_crack.c_str()) == 0){
                 is_found = true;
-                std::cout << "pwd found " << pwd << ":" << hash << std::endl;
+                std::cout << "pwd found " << pwd << ":" << hash_to_crack << std::endl;
                 break;
             }
         }
